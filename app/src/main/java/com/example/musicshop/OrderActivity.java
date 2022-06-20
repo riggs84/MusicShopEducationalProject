766e8de810ff7ce5f51b75c3398c35dbad7198ae
viktoriurkov <VVYurkov@alfabank.ru>
2022-06-20 19:45:38 +0300

    feat(*): add order submit

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private String customerName;
+    private String itemName;
+    private int quantity;
+    private double price;
+    private double total;
+
+    private String[] addresses = { "azaza@mail.ru" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +30,33 @@ public class OrderActivity extends AppCompatActivity {
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         TextView priceTextView = findViewById(R.id.priceTextView);
         TextView orderPriceTextView = findViewById(R.id.oderPriceTextView);
-        Log.d("azaza", receivedIntent.getStringExtra("itemName"));
 
-        customerNameTextView.setText("Customer name: " + receivedIntent.getStringExtra("customerName"));
-        itemNameTextView.setText("Goods name: " + receivedIntent.getStringExtra("itemName"));
-        quantityTextView.setText("Quantity: " + receivedIntent.getIntExtra("quantity", 0));
-        priceTextView.setText("Price: " + receivedIntent.getDoubleExtra("price", 0.0));
-        orderPriceTextView.setText("Total: " + receivedIntent.getDoubleExtra("total", 0.0));
+        customerName = receivedIntent.getStringExtra("customerName");
+        itemName = receivedIntent.getStringExtra("itemName");
+        quantity = receivedIntent.getIntExtra("quantity", 0);
+        price = receivedIntent.getDoubleExtra("price", 0.0);
+        total = receivedIntent.getDoubleExtra("total", 0.0);
+
+        customerNameTextView.setText("Customer name: " + customerName);
+        itemNameTextView.setText("Goods name: " + itemName);
+        quantityTextView.setText("Quantity: " + quantity);
+        priceTextView.setText("Price: " + price);
+        orderPriceTextView.setText("Total: " + total);
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Customer order");
+        intent.putExtra(Intent.EXTRA_TEXT, getOrderBody());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private String getOrderBody() {
+        return "Customer Name: " + customerName + "\n" + "Item Name: " + itemName + "\n" + "Quantity: "
+                + quantity + "\n" + "Price: " + price + "\n" + "Total: " + total;
     }
 }
